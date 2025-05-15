3bea9c7098989573ac30378893051a7e5ad1abe6
Jyoshitha Pechetti <jyoshithapechetti03@gmail.com>
2025-05-15 22:10:56 +0530

    Update ride.js

--- js/ride.js
+++ js/ride.js
@@ -1,11 +1,11 @@
-/*global HitchHop _config*/
+/*global WildRydes _config*/
 
-var HitchHop = window.HitchHop || {};
-HitchHop.map = HitchHop.map || {};
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
 
 (function rideScopeWrapper($) {
     var authToken;
-    HitchHop.authToken.then(function setAuthToken(token) {
+    WildRydes.authToken.then(function setAuthToken(token) {
         if (token) {
             authToken = token;
         } else {
@@ -15,7 +15,7 @@ HitchHop.map = HitchHop.map || {};
         alert(error);
         window.location.href = '/signin.html';
     });
-    function requestCab(pickupLocation) {
+    function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
             url: _config.api.invokeUrl + '/ride',
@@ -33,21 +33,21 @@ HitchHop.map = HitchHop.map || {};
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
-                alert('An error occured when requesting your Cab:\n' + jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
             }
         });
     }
 
     function completeRequest(result) {
-        var Cab;
+        var unicorn;
         var pronoun;
         console.log('Response received from API: ', result);
-        Cab = result.Cab;
-        pronoun = Cab.Gender === 'Male' ? 'his' : 'her';
-        displayUpdate(Cab.Name + ', your ' + Cab.Color + ' Cab, is on ' + pronoun + ' way.');
+        unicorn = result.Unicorn;
+        pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
+        displayUpdate(unicorn.Name + ', your cab, is on ' + pronoun + ' way.');
         animateArrival(function animateCallback() {
-            displayUpdate(Cab.Name + ' has arrived. Giddy up!');
-            HitchHops.map.unsetLocation();
+            displayUpdate(unicorn.Name + ' has arrived. Hurry up!');
+            WildRydes.map.unsetLocation();
             $('#request').prop('disabled', 'disabled');
             $('#request').text('Set Pickup');
         });
@@ -56,9 +56,9 @@ HitchHop.map = HitchHop.map || {};
     // Register click handler for #request button
     $(function onDocReady() {
         $('#request').click(handleRequestClick);
-        $(HitchHop.map).on('pickupChange', handlePickupChanged);
+        $(WildRydes.map).on('pickupChange', handlePickupChanged);
 
-        HitchHop.authToken.then(function updateAuthMessage(token) {
+        WildRydes.authToken.then(function updateAuthMessage(token) {
             if (token) {
                 displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
                 $('.authToken').text(token);
@@ -77,28 +77,28 @@ HitchHop.map = HitchHop.map || {};
     }
 
     function handleRequestClick(event) {
-        var pickupLocation = HitchHop.map.selectedPoint;
+        var pickupLocation = WildRydes.map.selectedPoint;
         event.preventDefault();
-        requestCab(pickupLocation);
+        requestUnicorn(pickupLocation);
     }
 
     function animateArrival(callback) {
-        var dest = HitchHop.map.selectedPoint;
+        var dest = WildRydes.map.selectedPoint;
         var origin = {};
 
-        if (dest.latitude > HitchHop.map.center.latitude) {
-            origin.latitude = HitchHop.map.extent.minLat;
+        if (dest.latitude > WildRydes.map.center.latitude) {
+            origin.latitude = WildRydes.map.extent.minLat;
         } else {
-            origin.latitude = HitchHop.map.extent.maxLat;
+            origin.latitude = WildRydes.map.extent.maxLat;
         }
 
-        if (dest.longitude > HitchHop.map.center.longitude) {
-            origin.longitude = HitchHop.map.extent.minLng;
+        if (dest.longitude > WildRydes.map.center.longitude) {
+            origin.longitude = WildRydes.map.extent.minLng;
         } else {
-            origin.longitude = HitchHop.map.extent.maxLng;
+            origin.longitude = WildRydes.map.extent.maxLng;
         }
 
-        HitchHop.map.animate(origin, dest, callback);
+        WildRydes.map.animate(origin, dest, callback);
     }
 
     function displayUpdate(text) {
